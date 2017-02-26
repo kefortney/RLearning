ebc5fc41e7f4961b5938dabfb30ea11a1ec2ae89
kefortney <kefortney@gmail.com>
2017-02-26 11:02:11 -0500

    Creation of housing data r script

---
+++ HousingData.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+library(plyr)
+library(dplyr)
+library(tidyr)
+
+#get the housingdata
+
+wholedf <- read.csv("C:/Users/kefor/Desktop/RLearning/BurlingtonPropertyDetails.csv", header = TRUE)
+
+str(wholedf)
